91e3854c9be6737dcaf1ea3528173c2a0ed82f47
Pavel Naumovets <pavel.naumovets@mail.ru>
2023-04-16 22:31:46 +0300

    homework 6. Pattern DataMapper.

--- src/main/java/com/naumovets/patterns/Application.java
+++ src/main/java/com/naumovets/patterns/Application.java
@@ -27,63 +27,62 @@ public class Application {
          Использование порождающего паттерна builder на примере объекта для передачи
          данных заказа из домашнего проекта интернет-магазина
         */
-//        List<OrderItemDto> orderLine = new ArrayList<>();
-//
-//        OrderItemDto orderItemDto = new OrderItemDto(
-//                1L,
-//                "Молоко",
-//                1L,
-//                10,
-//                BigDecimal.valueOf(100),
-//                BigDecimal.valueOf(1000)
-//        );
-//
-//        orderLine.add(orderItemDto);
-//
-//        OrderDto orderDto = builder()
-//                .id(1L)
-//                .username("Николай")
-//                .items(orderLine)
-//                .address("г.Москва")
-//                .phone("+7(923)983-34-56")
-//                .totalPrice(BigDecimal.valueOf(1000))
-//                .sequenceNumber(1)
-//                .build();
-//
-//        //Убедимся, что объект создан корректно
-//        System.out.println(orderDto.toString());
-//
-//        //=====================================================
-//        // Pattern Facade - структурный паттерн
-//        //=====================================================
-//
-//        /*
-//         Facade - паттерн задача которого предоставить простой интерфейс и скрыть сложную реализацию от пользователя.
-//         Реализуется с помощью интерфейса и класса реализующего этот интерфейс. В проекте интернет-магазина был применен
-//         паатер facade для валидации данных из формы для заказа. Данные содержат информацию о пользователе и заказе, которую
-//         нужно проверить на корректность написания (имя, телефон, адрес). При этом использование facade позволяет применить
-//         динамическое связывание ссылки с кодом и подставить нужную реализацию методов во время компиляции.
-//        */
-//        OrderValidator orderValidator = new OrderValidatorImpl();
-//        orderValidator.validate("Ivan", "8(911)295-96-84", "Россия,г.Москва,ул.Нахимова,дом 2,кв.18");
-//
-//        //=====================================================
-//        // Pattern Iterator - поведенческий паттерн
-//        //=====================================================
-//
-//        /*
-//         Iterator - паттерн задача которого предоставьте интерфейс для перебора элементов. В проекте интернет-магазина
-//         применял для перебора элементов коллекции ArrayList<>() в виде for each метода, который является синтаксическим
-//         сахаром и по сути реализован через Iterator.
-//        */
-//
-//        List<String> list = new ArrayList<>(List.of("word", "inside", "list"));
-//        Iterator<String> iterator = new ListIterator<>(list);
-//
-//        while (iterator.hasNext()) {
-//            String element = iterator.next();
-//            System.out.println(element);
-//        }
+        List<OrderItemDto> orderLine = new ArrayList<>();
+
+        OrderItemDto orderItemDto = new OrderItemDto(
+                1L,
+                "Молоко",
+                1L,
+                10,
+                BigDecimal.valueOf(100),
+                BigDecimal.valueOf(1000)
+        );
+
+        orderLine.add(orderItemDto);
+
+        OrderDto orderDto = builder()
+                .id(1L)
+                .username("Николай")
+                .items(orderLine)
+                .address("г.Москва")
+                .phone("+7(923)983-34-56")
+                .totalPrice(BigDecimal.valueOf(1000))
+                .sequenceNumber(1)
+                .build();
+
+        System.out.println(orderDto.toString());
+
+        //=====================================================
+        // Pattern Facade - структурный паттерн
+        //=====================================================
+
+        /*
+         Facade - паттерн задача которого предоставить простой интерфейс и скрыть сложную реализацию от пользователя.
+         Реализуется с помощью интерфейса и класса реализующего этот интерфейс. В проекте интернет-магазина был применен
+         паатер facade для валидации данных из формы для заказа. Данные содержат информацию о пользователе и заказе, которую
+         нужно проверить на корректность написания (имя, телефон, адрес). При этом использование facade позволяет применить
+         динамическое связывание ссылки с кодом и подставить нужную реализацию методов во время компиляции.
+        */
+        OrderValidator orderValidator = new OrderValidatorImpl();
+        orderValidator.validate("Ivan", "8(911)295-96-84", "Россия,г.Москва,ул.Нахимова,дом 2,кв.18");
+
+        //=====================================================
+        // Pattern Iterator - поведенческий паттерн
+        //=====================================================
+
+        /*
+         Iterator - паттерн задача которого предоставьте интерфейс для перебора элементов. В проекте интернет-магазина
+         применял для перебора элементов коллекции ArrayList<>() в виде for each метода, который является синтаксическим
+         сахаром и по сути реализован через Iterator.
+        */
+
+        List<String> list = new ArrayList<>(List.of("word", "inside", "list"));
+        Iterator<String> iterator = new ListIterator<>(list);
+
+        while (iterator.hasNext()) {
+            String element = iterator.next();
+            System.out.println(element);
+        }
 
         //====================================================================
         // Pattern DataMapper и IdentityMap - архитектурные системные паттерны
